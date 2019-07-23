4861a11375be767a6275021da34f464f9a38080e
Rory Murray <rory.christian.murray@gmail.com>
2019-07-23 17:38:50 -0400

    Create CLEAR_TODO action

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -13,20 +13,23 @@ export const initialState = [
 
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
+export const CLEAR_TODO = "CLEAR_TODO";
 
 export function reducer(state, action) {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       const newTodo = {
         item: action.payload,
         completed: false,
         id: Date.now()
       };
       return [...state, newTodo];
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       console.log("TOGGLE_TODO action.payload", action.payload);
       return action.payload;
+      case: CLEAR_TODO:
+      return state
     default:
-      return state;
+      return state.filter(todo => !todo.completed)
   }
 }
